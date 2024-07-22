d9fb0133f83020febcf738b9d288c9cb9215042f
yash shrestha <yashcrest@gmail.com>
2024-07-22 11:26:44 +1000

    header.tsx file added.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,6 +16,9 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+      </div>
+      <div>
+        <Header title="This is header from app.jsx file" color="black" />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
